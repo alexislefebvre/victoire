93145228aa3da76a14f355b60f1250b573941c8c
Alexis Lefebvre <alexislefebvre+github@gmail.com>
2017-08-02 23:38:02 +0200

    test 2

--- Bundle/TwigBundle/Controller/ExceptionController.php
+++ Bundle/TwigBundle/Controller/ExceptionController.php
@@ -4,13 +4,19 @@ namespace Victoire\Bundle\TwigBundle\Controller;
 
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\TwigBundle\Controller\ExceptionController as BaseExceptionController;
-use Symfony\Component\Debug\Exception\FlattenException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 use Symfony\Component\Routing\Router;
+
+// BC for Symfony ≤ 2.8
+if (class_exists('Symfony\Component\HttpKernel\Exception\FlattenException')) {
+    class_alias('Symfony\Component\HttpKernel\Exception\FlattenException', 'BaseFlattenException');
+} else {
+    class_alias('Symfony\Component\Debug\Exception\FlattenException', 'BaseFlattenException');
+}
 
 /**
  * Redirects to a victoire error page when facing a Flatted Exception.
@@ -65,7 +71,7 @@ class ExceptionController extends BaseExceptionController
      *
      * @return Response
      */
-    public function showAction(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null)
+    public function showAction(Request $request, BaseFlattenException $exception, DebugLoggerInterface $logger = null, $_format = 'html')
     {
         $currentContent = $this->getAndCleanOutputBuffering($request->headers->get('X-Php-Ob-Level', -1));
         $code = $exception->getStatusCode();
@@ -90,7 +96,7 @@ class ExceptionController extends BaseExceptionController
         }
 
         return new Response($this->twig->render(
-            $this->findTemplate($request, 'html', $code, $this->debug),
+            $this->findTemplate($request, $_format, $code, $this->debug),
             [
                 'status_code'    => $code,
                 'status_text'    => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : '',
